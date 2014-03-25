7820ca7bad6b8dd22c49a48c4cb02dc37c68b87b
Max Prokopiev <me@maxprokopiev.com>
2014-03-26 00:15:16 +0400

    Add patch to properly indent coffeescript

--- core/coffeescript.el
+++ core/coffeescript.el
@@ -1,3 +1,11 @@
 (setq coffee-tab-width 2)
 
+;; properly indent with evil (see http://stackoverflow.com/questions/18313914/how-to-remap-a-function-to-another-in-emacs/18333484#18333484)
+(defadvice coffee-indent-line (after wants-indent activate)
+  (let ((tabs-needed (- (/ (coffee-previous-indent) coffee-tab-width) 1)))
+    (when (> tabs-needed 0)
+      (insert-tab tabs-needed)))
+  (when (coffee-line-wants-indent)
+    (insert-tab)))
+
 (provide 'coffeescript)
