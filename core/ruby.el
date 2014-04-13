09a06a7327e6b646a5e75b5cbb8bc5aaa49c8f61
Max Prokopiev <me@maxprokopiev.com>
2014-04-13 21:22:53 +0400

    Do not add magic encoding comment in ruby

--- core/ruby.el
+++ core/ruby.el
@@ -14,6 +14,8 @@
 (add-to-list 'auto-mode-alist '("\\.jsonify\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Podfile\\'" . ruby-mode))
 
+(setq ruby-insert-encoding-magic-comment nil)
+
 (add-hook 'ruby-mode-hook
   (function (lambda ()
           (setq evil-shift-width ruby-indent-level))))
